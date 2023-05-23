b8ef0a3791084fc61183d5a7b33584771e128b0d
Kyle Zhu <kalamazhu@fb.com>
2023-05-22 23:47:02 -0700

    Initial Commit

---
+++ Simple iOS app/Simple_iOS_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_iOS_appApp.swift
+//  Simple iOS app
+//
+//  Created by kyle Zhu on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Simple_iOS_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Simple iOS appUITests/Simple_iOS_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Simple_iOS_appUITestsLaunchTests.swift
+//  Simple iOS appUITests
+//
+//  Created by kyle Zhu on 5/22/23.
+//
+
+import XCTest
+
+final class Simple_iOS_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
